4cd344e445967a30f7af28d86b80b13dba289d66
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-11-20 14:56:32 +0200

    Initial Commit

---
+++ Spirograph/SpirographApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirographApp.swift
+//  Spirograph
+//
+//  Created by Vahtee Boo on 20.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
